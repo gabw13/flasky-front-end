177a2e47cf38414e9bd9eed5207a5ec2705f7d0e
Gabriela Webb <gabw813@gmail.com>
2022-06-16 11:31:58 -0700

    adds state

--- src/components/Driver.js
+++ src/components/Driver.js
@@ -1,8 +1,22 @@
 import "./Driver.css";
 import PropTypes from "prop-types";
 // one to one mapping between functions and components; needs to return jsx
+import { useState } from "react";
 
 const Driver = (props) => {
+  const [handsome, setHandsome] = useState(props.handsome);
+  // defining a function that will correspond to button
+  const flipHandsomeness = () => {
+    if (handsome) {
+      setHandsome(false);
+      // do NOT say handsome = false // NO
+    } else {
+      setHandsome(true);
+    }
+  };
+
+  const [country, setCountry] = useState(props.country);
+
   // const tsunoda = {
   //   name: "Yuki Tsunoda",
   //   team: "Alpha Tauri",
@@ -10,13 +24,21 @@ const Driver = (props) => {
   //   handsome: true,
   // };
 
+  const changeCountryName = (event) => {
+    setCountry(event.target.value);
+  };
+
   return (
     <div>
       <h2 className="driver__name"> {props.name}</h2>
       <ul>
         <li>Team: {props.team}</li>
-        <li>Country: {props.country}</li>
-        <li>Handsome: {props.handsome.toString()}</li>
+        <li>Country: {country}</li>
+        <li>Handsome: {handsome.toString()}</li>
+        {/* // using variable from line 7 because if you use props.handsome here it will always be whats in the array */}
+        <button onClick={flipHandsomeness}>change handsomeness</button>
+        Set Country
+        <input type="text" value={country} onChange={changeCountryName}></input>
       </ul>
     </div>
   );
